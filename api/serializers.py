f5b4e0232fb32bd68a747c0a3eccaac104d0a951
anassu1400 <anassu1400@gmail.com>
2019-02-20 14:53:10 +0300

    Anas, Nadia

--- api/serializers.py
+++ api/serializers.py
@@ -2,25 +2,56 @@ from rest_framework import serializers
 from restaurants.models import Restaurant
 
 class RestaurantListSerializer(serializers.ModelSerializer):
+    detail = serializers.HyperlinkedIdentityField(
+        view_name = "api-detail",
+        lookup_field = "id",
+        lookup_url_kwarg = "restaurant_id"
+        )
+    update = serializers.HyperlinkedIdentityField(
+        view_name = "api-update",
+        lookup_field = "id",
+        lookup_url_kwarg = "restaurant_id"
+        )
+    delete = serializers.HyperlinkedIdentityField(
+        view_name = "api-delete",
+        lookup_field = "id",
+        lookup_url_kwarg = "restaurant_id"
+        )
     class Meta:
         model = Restaurant
         fields = [
             'name',
             'opening_time',
             'closing_time',
+            'detail',
+            'update',
+            'delete',
             ]
 
 
 class RestaurantDetailSerializer(serializers.ModelSerializer):
+    update = serializers.HyperlinkedIdentityField(
+        view_name = "api-update",
+        lookup_field = "id",
+        lookup_url_kwarg = "restaurant_id"
+        )
+    delete = serializers.HyperlinkedIdentityField(
+        view_name = "api-delete",
+        lookup_field = "id",
+        lookup_url_kwarg = "restaurant_id"
+        )
     class Meta:
         model = Restaurant
         fields = [
             'id',
+            'update',
+            'delete',
             'owner',
             'name',
             'description',
             'opening_time',
             'closing_time',
+            
             ]
 
 class RestaurantCreateUpdateSerializer(serializers.ModelSerializer):
